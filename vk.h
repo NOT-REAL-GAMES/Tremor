05f13b85c921518c5b4e7fca33a824519e30f16b
Árbjartur Kristjánsson <53384860+rbwldrf@users.noreply.github.com>
2025-05-20 16:44:14 +0000

    just to make it more obvious

--- vk.h
+++ vk.h
@@ -4640,12 +4640,15 @@ namespace tremor::gfx {
             auto currentTime = std::chrono::high_resolution_clock::now();
             float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
 
+            cam.setPosition(glm::vec3(sin(time*1.5f)*4, 0.0f, cos(time)*4));
+            cam.lookAt(glm::vec3(0.0f, 0.0f, 0.0f));
+
             cam.update(0.0f);
 
             UniformBufferObject ubo{};
 
             // Create a simple model matrix (rotate the quad over time)
-            ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(45.0f), glm::vec3(2.0f, 0.0f, 1.0f));
+            ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 
             // Create a view matrix (look at the quad from a slight distance)
             ubo.view = cam.getViewMatrix();
